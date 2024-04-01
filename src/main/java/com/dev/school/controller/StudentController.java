282166299a6dcf55d50b2146c9b8359d7039e841
DOVENE AGOGUE <adovene@gmail.com>
2024-04-01 08:50:48 +0200

    add web mvc with crud actions on in memory list

---
+++ src/main/java/com/dev/school/controller/StudentController.java
@@ -0,0 +1,58 @@
+package com.dev.school.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dev.school.model.Student;
+import com.dev.school.service.StudentService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("students", studentService.getAll());
+        return "student/list";
+    }
+
+    @GetMapping("/add")
+    public String displayAddForm(Student student) {
+        return "student/add";
+    }
+
+    @PostMapping("/add")
+    public String processAdd(Model model, Student student) {
+        studentService.add(student);
+        return "redirect:/student/list";
+    }
+
+     @GetMapping("/update/{id}")
+    public String displayUpdateForm(@PathVariable("id") String id, Model model) {
+        Student student = studentService.findById(id);
+        model.addAttribute("student", student);
+        return "student/update";
+    }
+
+    @PostMapping("/update")
+    public String processUpdate(Student student, Model model) {
+        studentService.update(student);
+        return "redirect:/student/list";
+    }
+  
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") String id) {
+        studentService.delete(id);
+        return "redirect:/student/list";
+    }
+    
+}
---
+++ src/main/java/com/dev/school/service/StudentService.java
@@ -0,0 +1,41 @@
+package com.dev.school.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.school.model.Student;
+import com.dev.school.repository.StudentRepository;
+
+@Service
+public class StudentService {
+ 
+    @Autowired 
+    StudentRepository studentRepository;
+public List<Student> getAll(){
+        return studentRepository.getAll();
+    }
+
+    public void add(Student student){
+        studentRepository.add(student);
+    }
+
+    public void update(Student studentParameter){
+        studentRepository.update(studentParameter);
+    }
+
+    public void delete(String id){
+        studentRepository.delete(id);
+    }
+
+    public Student findById(String id){
+        Optional<Student> optionalStudent = studentRepository.findById(id);
+        if (optionalStudent.isPresent()) {
+            return optionalStudent.get();
+        }
+        return null;
+    }
+
+}
